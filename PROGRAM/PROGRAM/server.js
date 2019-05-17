aa3ea625847ed97e74e9815f5df6b3f9b47f6919
TTS25 <sangdonga25@gmail.com>
2019-05-17 16:17:26 +0700

    basic

--- PROGRAM/PROGRAM/server.js
+++ PROGRAM/PROGRAM/server.js
@@ -1,8 +1,50 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+﻿'use strict';
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var MongoClient = require('mongodb').MongoClient;
+
+var PORT = 1337;
+var db_URL = 'mongodb://localhost:27017';
+
+var server = express();
+
+const path = require('path');
+
+// parse application/x-www-form-urlencoded
+server.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+server.use(bodyParser.json())
+
+server.use(express.static(path.join(__dirname, '/styles')));
+server.use(express.static(path.join(__dirname, '/assets')));
+
+server.set('view engine', 'pug');
+server.set('views', './views')
+
+//Database Connect
+
+
+//console.log(list_User);
+
+server.get('/', (req, res) => {
+    res.render('login_Page');
+})
+
+server.get('/users', (req, res) => {
+    MongoClient.connect(db_URL, function (err, client) {
+        if (err) { return console.dir(err); }
+        var db = client.db('NODEJS_APP');
+        db.collection("Users").find({}).toArray(function (err, list_Users) {
+            if (err) throw err;
+            res.render('users_Page', { users: list_Users });
+            client.close();
+        });
+    });
+
+    
+})
+
+server.listen(PORT, () => console.log('Server is listening on ' + PORT));
